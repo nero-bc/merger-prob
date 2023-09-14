23ad5f39cb8b4d7aeada30dca6390d691423ef26
veer naal <140493234+viro-net@users.noreply.github.com>
2023-09-14 15:51:37 +0530

    Add files via upload

---
+++ plugins/mergeVideoSub.py
@@ -0,0 +1,197 @@
+import asyncio
+import os
+import time
+
+from bot import (
+    LOGGER,
+    SUBTITLE_EXTENSIONS,
+    UPLOAD_AS_DOC,
+    UPLOAD_TO_DRIVE,
+    VIDEO_EXTENSIONS,
+    delete_all,
+    formatDB,
+    gDict,
+    queueDB,
+)
+from config import Config
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from helpers.display_progress import Progress
+from helpers.ffmpeg_helper import MergeSubNew, take_screen_shot
+from helpers.rclone_upload import rclone_driver, rclone_upload
+from helpers.uploader import uploadVideo
+from helpers.utils import UserSettings
+from PIL import Image
+from pyrogram import Client
+from pyrogram.errors import MessageNotModified
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.errors.rpc_error import UnknownError
+from pyrogram.types import CallbackQuery, Message
+
+
+async def mergeSub(c: Client, cb: CallbackQuery, new_file_name: str):
+    omess = cb.message.reply_to_message
+    vid_list = list()
+    await cb.message.edit("🛂 Processing...")
+    duration = 0
+    video_mess = queueDB.get(cb.from_user.id)["videos"][0]
+    list_message_ids: list = queueDB.get(cb.from_user.id)["subtitles"]
+    list_message_ids.insert(0, video_mess)
+    list_message_ids.sort()
+    if list_message_ids is None:
+        await cb.answer("Queue Empty", show_alert=True)
+        await cb.message.delete(True)
+        return
+    if not os.path.exists(f"downloads/{str(cb.from_user.id)}/"):
+        os.makedirs(f"downloads/{str(cb.from_user.id)}/")
+    msgs: list[Message] = await c.get_messages(
+        chat_id=cb.from_user.id, message_ids=list_message_ids
+    )
+    all = len(msgs)
+    n=1
+    for i in msgs:
+        media = i.video or i.document
+        await cb.message.edit(f"🛂 Starting Download Of ... `{media.file_name}`")
+        LOGGER.info(f"🛂 Starting Download Of ... {media.file_name}")
+        currentFileNameExt = media.file_name.rsplit(sep=".")[-1].lower()
+        if currentFileNameExt in VIDEO_EXTENSIONS:
+            tmpFileName = "vid.mkv"
+        elif currentFileNameExt in SUBTITLE_EXTENSIONS:
+            tmpFileName = "sub." + currentFileNameExt
+        await asyncio.sleep(5)
+        file_dl_path = None
+        try:
+            c_time = time.time()
+            prog = Progress(cb.from_user.id, c, cb.message)
+            file_dl_path = await c.download_media(
+                message=media,
+                file_name=f"downloads/{str(cb.from_user.id)}/{str(i.id)}/{tmpFileName}",
+                progress=prog.progress_for_pyrogram,
+                progress_args=(f"🛂 Downloading: `{media.file_name}`", c_time,f"\n**Downloading: {n}/{all}**"),
+            )
+            n+=1
+            if gDict[cb.message.chat.id] and cb.message.id in gDict[cb.message.chat.id]:
+                return
+            await cb.message.edit(f"🛂 Downloaded Sucessfully ... `{media.file_name}`")
+            LOGGER.info(f"Downloaded Sucessfully ... {media.file_name}")
+            await asyncio.sleep(5)
+        except Exception as downloadErr:
+            LOGGER.warning(f"🚮 Failed To Download Error: {downloadErr}")
+            queueDB.get(cb.from_user.id)["subtitles"].remove(i.id)
+            await cb.message.edit("🛃 File Skipped!")
+            await asyncio.sleep(4)
+            await cb.message.delete(True)
+            continue
+        vid_list.append(f"{file_dl_path}")
+
+    subbed_video = MergeSubNew(
+        filePath=vid_list[0],
+        subPath=vid_list[1],
+        user_id=cb.from_user.id,
+        file_list=vid_list,
+    )
+    _cache = list()
+    if subbed_video is None:
+        await cb.message.edit("🚮 Failed To Add Subs Video !")
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        return
+    try:
+        await cb.message.edit("🛅 Sucessfully Muxed Video !")
+    except MessageNotModified:
+        await cb.message.edit("🛅 Sucessfully Muxed Video !")
+    LOGGER.info(f"Video muxed for: {cb.from_user.first_name} ")
+    await asyncio.sleep(3)
+    file_size = os.path.getsize(subbed_video)
+    os.rename(subbed_video, new_file_name)
+    await cb.message.edit(
+        f"♿ Renaming Video To\n **{new_file_name.rsplit('/',1)[-1]}**"
+    )
+    await asyncio.sleep(3)
+    merged_video_path = new_file_name
+    if UPLOAD_TO_DRIVE[f"{cb.from_user.id}"]:
+        await rclone_driver(omess, cb, merged_video_path)
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        return
+    if file_size > 2044723200 and Config.IS_PREMIUM == False:
+        await cb.message.edit(
+            f"♿ Video Is Larger Than 2GB Can't Upload,\n\n Tell {Config.OWNER_USERNAME} To Add Premium Account To Get 4GB TG Uploads"
+        )
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        return
+    if Config.IS_PREMIUM and file_size > 4241280205:
+        await cb.message.edit(
+            f"♿ Video is Larger Than 4GB Can't Upload,\n\n Tell {Config.OWNER_USERNAME} To Die With Premium Account"
+        )
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        return
+    await cb.message.edit("🛃 Extracting Video Data ...")
+
+    duration = 1
+    try:
+        metadata = extractMetadata(createParser(merged_video_path))
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
+    except Exception as er:
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        await cb.message.edit("🚮 Merged Video Is Corrupted")
+        return
+    try:
+        user = UserSettings(cb.from_user.id, cb.from_user.first_name)
+        thumb_id = user.thumbnail
+        if thumb_id is None:
+            raise Exception
+        # thumb_id = await database.getThumb(cb.from_user.id)
+        video_thumbnail = f"downloads/{str(cb.from_user.id)}_thumb.jpg"
+        await c.download_media(message=str(thumb_id), file_name=video_thumbnail)
+    except Exception as err:
+        LOGGER.info("Generating thumb")
+        video_thumbnail = await take_screen_shot(
+            merged_video_path, f"downloads/{str(cb.from_user.id)}", (duration / 2)
+        )
+    width = 1280
+    height = 720
+    try:
+        thumb = extractMetadata(createParser(video_thumbnail))
+        height = thumb.get("height")
+        width = thumb.get("width")
+        img = Image.open(video_thumbnail)
+        if width > height:
+            img.resize((320, height))
+        elif height > width:
+            img.resize((width, 320))
+        img.save(video_thumbnail)
+        Image.open(video_thumbnail).convert("RGB").save(video_thumbnail, "JPEG")
+    except:
+        await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+        queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+        formatDB.update({cb.from_user.id: None})
+        await cb.message.edit(
+            "🛃 Merged Video Is Corrupted \n\n<i>Try Setting Custom Thumbnail</i>",
+        )
+        return
+    await uploadVideo(
+        c=c,
+        cb=cb,
+        merged_video_path=merged_video_path,
+        width=width,
+        height=height,
+        duration=duration,
+        video_thumbnail=video_thumbnail,
+        file_size=os.path.getsize(merged_video_path),
+        upload_mode=UPLOAD_AS_DOC[f"{cb.from_user.id}"],
+    )
+    await cb.message.delete(True)
+    await delete_all(root=f"downloads/{str(cb.from_user.id)}")
+    queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
+    formatDB.update({cb.from_user.id: None})
+    return
---
+++ plugins/usettings.py
@@ -0,0 +1,81 @@
+import time
+from pyrogram import filters, Client as mergeApp
+from pyrogram.types import Message, InlineKeyboardMarkup
+from helpers.msg_utils import MakeButtons
+from helpers.utils import UserSettings
+
+
+@mergeApp.on_message(filters.command(["settings"]))
+async def f1(c: mergeApp, m: Message):
+    # setUserMergeMode(uid=m.from_user.id,mode=1)
+    replay = await m.reply(text="Please wait", quote=True)
+    usettings = UserSettings(m.from_user.id, m.from_user.first_name)
+    await userSettings(
+        replay, m.from_user.id, m.from_user.first_name, m.from_user.last_name, usettings
+    )
+
+
+async def userSettings(
+    editable: Message,
+    uid: int,
+    fname,
+    lname,
+    usettings: UserSettings,
+):
+    b = MakeButtons()
+    if usettings.user_id:
+        if usettings.merge_mode == 1:
+            userMergeModeId = 1
+            userMergeModeStr = "📽️ Video + Video 📽️"
+        elif usettings.merge_mode == 2:
+            userMergeModeId = 2
+            userMergeModeStr = "📽️ Video + Audio 📼"
+        elif usettings.merge_mode == 3:
+            userMergeModeId = 3
+            userMergeModeStr = "📽️ Video + Subtitle 📜"
+        elif usettings.merge_mode == 4:
+            userMergeModeId = 4
+            userMergeModeStr = "🪢 Extract 🪢" 
+        if usettings.edit_metadata:
+            editMetadataStr = "🫡"
+        else:
+            editMetadataStr = "🙅‍♂️"
+        uSettingsMessage = f"""
+<b><u>Merge Bot settings for <a href='tg://user?id={uid}'>{fname} {lname}</a></u></b>
+    ┃
+    ┣**ID: <u>{usettings.user_id}</u>**
+    ┣**{'🎍' if usettings.banned else '🎍'} Ban Status: <u>{usettings.banned}</u>**
+    ┣**{'🎭' if usettings.allowed else '🎭'} Allowed: <u>{usettings.allowed}</u>**
+    ┣**{'🫡' if usettings.edit_metadata else '🙅‍♂️'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
+    ┗**🚏 Merge mode 🚏: <u>{userMergeModeStr}</u>**
+"""
+        markup = b.makebuttons(
+            [
+                "Merge mode",
+                userMergeModeStr,
+                "Edit Metadata",
+                editMetadataStr,
+                "Close",
+            ],
+            [
+                "tryotherbutton",
+                f"ch@ng3M0de_{uid}_{(userMergeModeId%4)+1}",
+                "tryotherbutton",
+                f"toggleEdit_{uid}",
+                "close",
+            ],
+            rows=2,
+        )
+        res = await editable.edit(
+            text=uSettingsMessage, reply_markup=InlineKeyboardMarkup(markup)
+        )
+    else:
+        usettings.name = fname
+        usettings.merge_mode = 1
+        usettings.allowed = False
+        usettings.edit_metadata = False
+        usettings.thumbnail = None
+        await userSettings(editable, uid, fname, lname, usettings)
+    # await asyncio.sleep(10)
+    # await c.delete_messages(chat_id=editable.chat.id, message_ids=[res.id-1,res.id])
+    return
