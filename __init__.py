99a8ed82510874f0b7f41a1d9901969c933ab7e5
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-11-17 07:48:15 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ MERGE_MODE = {}  # Maintain each user merge_mode
 UPLOAD_AS_DOC = {}  # Maintain each user ul_type
 UPLOAD_TO_DRIVE = {}  # Maintain each user drive_choice
 
-FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "█")
-UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
+FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "▣")
+UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "▢")
 EDIT_SLEEP_TIME_OUT = 10
 gDict = defaultdict(lambda: [])
 queueDB = {}
